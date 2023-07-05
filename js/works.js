0b3a627c924d07f41dd7b39902a29a0e7c9a282a
Yucel Olcay <yucelolcay@gmail.com>
2023-07-06 01:00:00 +0300

    Readme

--- js/works.js
+++ js/works.js
@@ -48,7 +48,7 @@ const portfolioList = [
       'Docker',
       'AWS',
     ],
-    linkToLive: 'https://timber-1209079008.us-east-1.elb.amazonaws.com/',
+    linkToLive: 'http://timbr-981687662.us-east-1.elb.amazonaws.com/',
   },
   {
     name: 'Show My Neighbors',
